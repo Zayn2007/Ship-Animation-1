741930fd16e1549e6510875f633428e96c986396
Zayn2007 <86636446+Zayn2007@users.noreply.github.com>
2021-07-24 18:45:32 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var ship, shipImg, sea, seaImg;
+function preload(){
+shipImg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+seaImg = loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea = createSprite(200,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -1;
+
+  ship = createSprite(100,300);
+  ship.addAnimation("ship",shipImg);
+  ship.scale = 0.2;
+}
+
+function draw() {
+  background("blue");
+  if(sea.x < 0){
+    sea.x = sea.width/5;
+  }
+ drawSprites();
+}
